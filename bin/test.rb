1a691292209e7c08002d8d83bc29c2d9d100fc0e
edxco <eduardo.baeza@yahoo.com>
2020-10-06 10:01:54 -0500

    First draft of the game complete

--- bin/test.rb
+++ bin/test.rb
@@ -1,12 +1,50 @@
-move = gets.chomp.to_i
- p move
+#!/usr/bin/env ruby
+require 'pry'
+require_relative "../lib/Player.rb"
+require_relative "../lib/game_initializer.rb"
+require_relative "../lib/table.rb"
 
- move <= 9 && move >= 1 ? state = false : state = true
+loop do
+players = game_initializer
+player_turn = players[0]
 
- p state
+table = Table.new
 
-  while state
-    puts "Invalid move, choose between 1 or 9?"
+table.show_table
+
+  game_moves = 9
+  game_on = true
+
+  while game_on
+    # Choose at random player for first move
+    puts "#{player_turn.name} (#{player_turn.sign}) your turn to choose move:"
     move = gets.chomp.to_i
-    move <= 9 && move >= 1 ? state = false : state = true
-  end 
+  
+    while table.valid_move(move)
+      puts "Invalid move, choose between 1 or 9?  :"
+      move = gets.chomp.to_i
+    end
+
+    table.show_table(move, player_turn.sign)
+
+    if table.check_win
+      puts "#{player_turn.name} Wins!" 
+      break
+    end 
+
+    game_moves -= 1
+    if game_moves.zero?
+      puts 'Game tie'
+      game_on = false
+    end
+
+    player_turn = player_turn == players[0] ? players[1] : players[0]
+  end
+
+  puts "Do you want to play again? (y/n):"
+  play_again = gets.chomp
+  break if play_again != "y"
+
+  end
+
+  puts "\nGame Over! Thank you for playing\nIf you have any suggestion to improve this game please open an Issue\nhttps://github.com/edxco/tic-tac-toe\n"
--- lib/table.rb
+++ lib/table.rb
@@ -1,8 +1,11 @@
 require 'pry'
 require_relative "game_initializer.rb"
 
-def table_method
-  table = {
+class Table
+  attr_accessor :table
+
+  def initialize
+  @table = {
     "1": 1,
     "2": 2,
     "3": 3,
@@ -13,5 +16,47 @@ def table_method
     "8": 8,
     "9": 9
   }
-  table
+  end
+
+  def show_table(move = nil, sign = nil)
+    @table[:"#{move}"] = sign
+    puts "     #{table[:"7"]} | #{table[:"8"]} | #{table[:"9"]}
+    ---+---+---
+     #{table[:"4"]} | #{table[:"5"]} | #{table[:"6"]}
+    ---+---+---
+     #{table[:"1"]} | #{table[:"2"]} | #{table[:"3"]}"
+  end
+
+  def valid_move(move)
+    check = @table[:"#{move}"]
+    check == "x" || check == "o" || move > 9 || move < 1 ? true : false
+  end
+
+  def check_win
+    if @table[:"1"] == @table[:"2"] && @table[:"3"] == @table[:"1"]
+      true
+      # puts 'Row bottom line'
+    elsif @table[:"4"] == @table[:"5"] && @table[:"4"] == @table[:"6"]
+      true
+      # puts 'Row middle line'
+    elsif @table[:"7"] == @table[:"8"] && @table[:"7"] == @table[:"9"]
+      true
+      # puts 'Row top line'
+    elsif @table[:"7"] == @table[:"4"] && @table[:"7"] == @table[:"1"]
+      true
+      # puts 'Col 1st line'
+    elsif @table[:"8"] == @table[:"5"] && @table[:"8"] == @table[:"2"]
+      true
+      # puts 'Col 2nd line'
+    elsif @table[:"9"] == @table[:"6"] && @table[:"9"] == @table[:"3"]
+      true
+      # puts 'Col 3rd line'
+    elsif @table[:"7"] == @table[:"5"] && @table[:"5"] == @table[:"3"]
+      true
+      # puts 'Diagonal'
+    elsif @table[:"1"] == @table[:"5"] && @table[:"5"] == @table[:"9"]
+      true
+      # puts 'Diagonal'
+    end
+  end
 end
